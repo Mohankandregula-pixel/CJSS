a9c26cee3dab3dd9baf9309cd884e3ebc94587ad
Mohan kandregula <71484692+Mohankandregula-pixel@users.noreply.github.com>
2021-11-17 09:53:37 +0530

    Update

--- Collections/src/SocialMedia1.java
+++ Collections/src/SocialMedia1.java
@@ -30,36 +30,45 @@ public class SocialMedia1 {
         List<Comments> commentsOfPost11 = new ArrayList<>();
 
 
-        Comments comment1 = new Comments("p1","m@gmail.com","Nice");
-        Comments comment2 = new Comments("p1","m@gmail.com","good");
+        Comments comment11 = new Comments("p11","m@gmail.com","Nice");
+        Comments comment12 = new Comments("p11","m@gmail.com","good");
 
-        commentsOfPost11.add(comment1);
-        commentsOfPost11.add(comment2);
+        commentsOfPost11.add(comment11);
+        commentsOfPost11.add(comment12);
 
         List<Comments> commentsOfPost12 = new ArrayList<>();
 
 
-        Comments comment21 = new Comments("p2","m@gmail.com","Nice");
-        Comments comment22 = new Comments("p2","m@gmail.com","good");
+        Comments comment121 = new Comments("p12","m@gmail.com","Nice");
+        Comments comment122 = new Comments("p12","m@gmail.com","good");
 
-        commentsOfPost12.add(comment21);
-        commentsOfPost12.add(comment22);
+        commentsOfPost12.add(comment121);
+        commentsOfPost12.add(comment122);
+
+        List<Comments> commentsOfPost21 = new ArrayList<>();
+
+
+        Comments comment21 = new Comments("p21","m@gmail.com","Nice");
+        Comments comment22 = new Comments("p21","m@gmail.com","good");
+
+        commentsOfPost21.add(comment21);
+        commentsOfPost21.add(comment22);
 
         List<Posts> postOfCustomer1 = new ArrayList<>();
 
-        Posts post1 = new Posts("p1","m@gamil.com","birthday","Celebration",commentsOfPost11);
-        Posts post2 = new Posts("p2","m@gamil.com","birthday","Celebration",commentsOfPost12);
+        Posts post11 = new Posts("p11","m@gamil.com","birthday","Celebration",commentsOfPost11);
+        Posts post12 = new Posts("p12","m@gamil.com","birthday","Celebration",commentsOfPost12);
 
-        postOfCustomer1.add(post1);
-        postOfCustomer1.add(post2);
+        postOfCustomer1.add(post11);
+        postOfCustomer1.add(post12);
 
         List<Posts> postOfCustomer2 = new ArrayList<>();
 
-        Posts post21 = new Posts("p1","m@gamil.com","birthday","Celebration",commentsOfPost11);
-        Posts post22 = new Posts("p2","m@gamil.com","birthday","Celebration",commentsOfPost12);
+        Posts post21 = new Posts("p21","m@gamil.com","birthday","Celebration",commentsOfPost21);
+//        Posts post22 = new Posts("p22","m@gamil.com","birthday","Celebration",commentsOfPost22);
 
-        postOfCustomer2.add(post1);
-        postOfCustomer2.add(post2);
+        postOfCustomer2.add(post21);
+//        postOfCustomer2.add(post22);
 
         Customer C1 = new Customer("Mohan","m@gamil.com",postOfCustomer1);
         Customer C2 = new Customer("nolan","n@gamil.com",postOfCustomer2);
@@ -69,6 +78,9 @@ public class SocialMedia1 {
         allCustomers.add(C1);
         allCustomers.add(C2);
 
+        for(Customer cust: allCustomers){
+            System.out.println(cust.getEmail());
+        }
 
     }
 }
